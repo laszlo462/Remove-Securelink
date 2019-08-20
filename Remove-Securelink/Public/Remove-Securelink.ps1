e071b8e02fcb4a9fc47669350cc24d369dec88d9
Steve Szabo <sszabo462@protonmail.com>
2019-08-20 16:41:45 -0500

    Remove-Securelink function code started, still WIP.

--- Remove-Securelink/Public/Remove-Securelink.ps1
+++ Remove-Securelink/Public/Remove-Securelink.ps1
@@ -1,14 +1,33 @@
 ﻿<#
 .SYNOPSIS
+    Remove SecureLink Connection Manager
+
 .DESCRIPTION
+    If an existing installation of the Citrix SecureLink Connection Manager is present on the system, it will be uninstalled.
 #>
-function Remove-Securelink {
+function Remove-Securelink
+{
     [CmdletBinding()]
     param(
 
     )
 
-    process {
+    process
+    {
+        # Initialize SecureLink path variable
+        $slPath = "C:\Users\$env:USERNAME\AppData\Local\SecureLink"
+
+        # First check that SecureLink is indeed present.
+        $slExists = Find-Securelink($slPath)
+
+        if ($slExists)
+        {
+            Invoke-Uninstall($slPath)
+        }
+        else
+        {
+            Write-Host "SecureLink not present"
+        }
 
     }
 }
--- Tests/Public/Remove-Securelink.Tests.ps1
+++ Tests/Public/Remove-Securelink.Tests.ps1
@@ -5,9 +5,12 @@ $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
 
 # To make test runable from project root, and from test directory itself. Do quick validation.
 $testsPath = Join-Path "Tests" "Public"
-if ((Get-Location).Path -match [Regex]::Escape($testsPath)) {
-    $psmPath = (Resolve-Path "..\..\Remove-Securelink\Remove-Securelink.psm1").Path    
-} else {
+if ((Get-Location).Path -match [Regex]::Escape($testsPath))
+{
+    $psmPath = (Resolve-Path "..\..\Remove-Securelink\Remove-Securelink.psm1").Path
+}
+else
+{
     $psmPath = (Resolve-Path ".\Remove-Securelink\Remove-Securelink.psm1").Path
 }
 
@@ -16,6 +19,12 @@ Import-Module $psmPath -Force -NoClobber
 InModuleScope "Remove-Securelink" {
 
     Describe "Remove-Securelink" {
+        It "should inform if SecureLink is not present" {
+
+        }
+        It "should execute Invoke-Uninstall if SecureLink is present" {
+
+        }
 
     }
 
