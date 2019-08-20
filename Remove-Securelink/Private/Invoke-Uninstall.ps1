cd424adb5068d7513bbbeb8a08f7032b94151485
Steve Szabo <sszabo462@protonmail.com>
2019-08-20 16:40:52 -0500

    Invoke-Uninstall function started, still WIP.

--- Remove-Securelink/Private/Invoke-Uninstall.ps1
+++ Remove-Securelink/Private/Invoke-Uninstall.ps1
@@ -1,14 +1,22 @@
 ﻿<#
 .SYNOPSIS
+    Invoke Uninstall
 .DESCRIPTION
+    Function to perform the SecureLink uninstallation if present on the system.
 #>
-function Invoke-Uninstall {
+function Invoke-Uninstall
+{
     [CmdletBinding()]
     param(
-
+        $slPath
     )
 
-    process {
-
+    process
+    {
+        Start-Process -FilePath "$slPath\SecureLinkCM.exe" -ArgumentList "-uninstall" -Wait -NoNewWindow
+        if (!Find-Securelink($slPath))
+        {
+            Write-Host "Success"
+        }
     }
 }
